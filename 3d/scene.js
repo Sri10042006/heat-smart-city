75ffb4e84559fef50c5605b8bda19f704aed4938
Sri10042006 <santhasrisubramani@gmail.com>
2026-02-18 10:26:14 +0530

    create html file

--- 3d/scene.js
+++ 3d/scene.js
@@ -1,1 +1,26 @@
+// Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x87ceeb);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.z = 20;
+camera.position.y = 10;
+camera.lookAt(0, 0, 0);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Light
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(10, 20, 10);
+scene.add(light);
 
